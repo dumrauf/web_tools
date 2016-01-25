96a0050f897914af672e09279dcd31d9fa9ca14b
Dominic Dumrauf <dominic.dumrauf.git@gmail.com>
2016-01-25 23:25:16 +0000

    Add some documentation around the util functions in 'image_converter'.

--- image_converter/utils/convert_image.py
+++ image_converter/utils/convert_image.py
@@ -4,16 +4,28 @@ from PIL.JpegImagePlugin import JpegImageFile
 
 __author__ = 'dominic'
 
+# A constant for the JPEG image format
 _FORMAT_JPEG = JpegImageFile.format
 
+# A list of image formats
 _IMAGE_FORMATS = [
     _FORMAT_JPEG
 ]
 
+# A dictionary of known image formats
 KNOWN_IMAGE_FORMATS = dict(zip(_IMAGE_FORMATS, _IMAGE_FORMATS))
 
 
 def _convert_image(image, format):
+    """
+    Converts the given 'image' to the supplied 'format' and returns the result.
+
+    Raises an exception in case the 'image' is not a valid image file.
+
+    :param image: the image to convert
+    :param format: the format to convert the image to
+    :return: the converted image as a StringIO object
+    """
     img = Image.open(image)
     converted_img = StringIO()
     img.save(converted_img, format=format)
@@ -21,5 +33,11 @@ def _convert_image(image, format):
 
 
 def convert_image_to_jpeg(image):
+    """
+    Converts the given 'image' to JPEG and returns it as an in-memory file
+
+    :param image: the image to convert
+    :return: the converted JPEG image as a StringIO object
+    """
     return _convert_image(image=image,
                           format=_FORMAT_JPEG)
--- image_converter/utils/filename.py
+++ image_converter/utils/filename.py
@@ -2,6 +2,18 @@ __author__ = 'dominic'
 
 
 def get_filename_without_extension(filename):
+    """
+    Returns the name of the 'filename', removing any extension. Here, an extension is indicated by a *dot*,
+    e.g. 'file.txt' where 'file' denotes the name and 'txt' is the extension.
+
+    If multiple extensions exist, only the last one is removed. In case no extension is present, the entire
+    'filename' is returned.
+
+    In case the 'filename' is empty, the string 'file' is returned.
+
+    :param filename: the filename to get the name from
+    :return: the name of the given 'filename'
+    """
     if not filename:
         return 'file'
     try:
